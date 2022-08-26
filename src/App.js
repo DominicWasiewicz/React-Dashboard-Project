f8bf2956d5fe9137ee353f330daf692a44408f84
DominicWasiewicz <dominic.wasiewicz@outlook.com>
2022-08-26 10:26:54 -0400

    feature implemented

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
-import React, {useEffect} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {FiSettings} from 'react-icons/fi';
-import {TooltipComponent} from '@syncfusion/ej2-react-popups';
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import {Navbar, Footer, Sidebar, ThemeSettings} from'./components';
-import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css'
 
+import { useStateContext } from './contexts/ContextProvider';
 export const App = () => {
-  const activeMenu = true;
-  
+  const { activeMenu } = useStateContext();
   return (
     <div>
-      
+
       <BrowserRouter>
-      <div className='flex relative dark:bg-main-dark-bg'>
-        <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
-          <TooltipComponent content="Settings" posistion="Top">
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" posistion="Top">
               <button type='button' className="text-3xl p-3 
               hover:drop-shadow-xl 
               hover:bg-light-gray 
               text-white"
-              style={{background: 'blue',
-              borderRadius: '50%'}}>
-                <FiSettings/>
+                style={{
+                  background: 'blue',
+                  borderRadius: '50%'
+                }}>
+                <FiSettings />
               </button>
             </TooltipComponent>
           </div>
           {activeMenu ? (
-           <div className='w-72 fixed sidebar
+            <div className='w-72 fixed sidebar
            dark:bg-secondary-dark-bg
            bg-white'>
               <Sidebar />
-            </div> 
+            </div>
           ) : (
-            <div className='w-0 dark:bg-secondary-dark-bg'> 
-               <Sidebar />
-            </div>  
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              <Sidebar />
+            </div>
           )}
           <div className={
             `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
           }>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-            <Navbar /> 
+              <Navbar />
             </div>
           </div>
 
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path="/" element={<Ecommerce/>} />
-              <Route path="/ecommerce" element={<Ecommerce/>} />
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
 
               {/* Pages */}
               <Route path="/orders" element={<Orders />} />
